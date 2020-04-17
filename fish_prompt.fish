950e164ca76b037d509b94240b3d69dc1533dc2f
stian <stian.meyer+git@gmail.com>
2020-04-17 19:42:31 +0200

    fish_prompt.fish

---
+++ fish_prompt.fish
@@ -0,0 +1,67 @@
+# fish theme: gentoo
+
+function _git_branch_name
+  echo (command git symbolic-ref HEAD ^/dev/null | sed -e 's|^refs/heads/||')
+end
+
+function _is_git_dirty
+  echo (command git status -s --ignore-submodules=dirty ^/dev/null)
+end
+
+function fish_prompt
+  set fish_greeting
+  set -l cyan (set_color -o cyan)
+  set -l red (set_color -o red)
+  set -l blue (set_color -o blue)
+  set -l green (set_color -o green)
+  set -l normal (set_color normal)
+
+  set -l cwd (pwd | sed -e "s!^$HOME!~!g")
+  # output the prompt, left to right:
+  if [ (id -u) = "0" ];
+    set cwd (basename $cwd)
+    # display host
+    echo -n -s $red (hostname|cut -d . -f 1) " "
+  else
+    # display 'user@host:'
+    echo -n -s $green (whoami) @ $green (hostname|cut -d . -f 1) " "
+  end
+
+  # display the current directory name:
+  echo -n -s $blue $cwd $normal
+
+  # show git branch and dirty state, if applicable:
+  if [ (_git_branch_name) ]
+    set -l git_branch '[' (_git_branch_name) ']'
+
+    if [ (_is_git_dirty) ]
+      set git_info $red $git_branch "×"
+    else
+      set git_info $cyan $git_branch
+    end
+    echo -n -s ' ' $git_info $normal
+  end
+
+  # terminate with a nice prompt char:
+  if [ (id -u) = "0" ];
+    set indicate '#'
+  else
+    set indicate '$'
+  end
+  echo -n -s $blue " $indicate " $normal
+end
+    function l
+    lsd -hA --group-dirs first
+end
+    function ex
+    exa -la
+end
+    function scolor
+    sh /home/stian/git/dt.dotcolorscripts/shell-color-scripts/colorscript.sh -r
+end
+    function ...
+    cd ../../..
+end
+    function ....
+    cd ../../../..
+end
