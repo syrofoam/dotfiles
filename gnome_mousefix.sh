93a3d86262ddbe29b7ac3b4f03dc79b85e85fdb0
stian <stian.meyer+git@gmail.com>
2021-05-08 21:54:00 +0200

    windows keybinds

--- gnome_mousefix.sh
+++ gnome_mousefix.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
+#mousefix
 gsettings set org.gnome.desktop.wm.preferences resize-with-right-button true
+
+#disable meta+1+2+3+4
+gsettings set org.gnome.shell.keybindings switch-to-application-N [1-4]
