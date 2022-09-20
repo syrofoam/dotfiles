6c551cc903cc3b4b88c22aa2d1a127822652fd05
syrofoam <stian.meyer+git@gmail.com>
2022-09-20 11:05:12 +0200

    missing fonts and unstable didnt apply so staying on 22.05 for now

--- configuration_laptop.nix
+++ configuration_laptop.nix
@@ -131,6 +131,7 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
+    nerdfonts
     ripgrep
     neofetch
     fd
@@ -146,6 +147,7 @@
     glib
     vim
     wget
+    font-manager
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
@@ -215,6 +217,6 @@
   # this value at the release version of the first install of this system.
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "unstable"; # Did you read the comment?
+  system.stateVersion = "22.05"; # Did you read the comment?
 
 }
