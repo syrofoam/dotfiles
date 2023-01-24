c41497af13a5027f8c97dc7f6eba3b9bea3da7a9
syrofoam <stian.meyer+git@gmail.com>
2023-01-24 20:22:05 +0100

    laptop nix configuration

--- configuration.nix
+++ configuration.nix
@@ -5,17 +5,26 @@
 { config, pkgs, ... }:
 
 {
+  nix = {
+    enable = true;
+    package = pkgs.nix;
+    settings.experimental-features = "nix-command flakes";
+  };
+
   imports =
     [ # Include the results of the hardware scan.
+      #./cpupower.nix
       ./hardware-configuration.nix
     ];
 
   # Bootloader.
+  #boot.kernelParams = [ "initcall_blacklist=acpi_cpufreq_init" ];
+  boot.kernelPackages = pkgs.linuxPackages_xanmod;
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.efi.efiSysMountPoint = "/boot/efi";
 
-  networking.hostName = "nixos"; # Define your hostname.
+  networking.hostName = "phobos"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
   # Configure network proxy if necessary
@@ -34,9 +43,9 @@
   # Enable the X11 windowing system.
   services.xserver.enable = true;
 
-  # Enable the XFCE Desktop Environment.
-  services.xserver.displayManager.lightdm.enable = true;
-  services.xserver.desktopManager.xfce.enable = true;
+  # Enable the KDE Plasma Desktop Environment.
+  services.xserver.displayManager.sddm.enable = true;
+  services.xserver.desktopManager.plasma5.enable = true;
 
   # Configure keymap in X11
   services.xserver = {
@@ -74,7 +83,12 @@
   users.users.stian = {
     isNormalUser = true;
     description = "stian";
-    extraGroups = [ "networkmanager" "wheel" ];
+    extraGroups = [ "networkmanager" "wheel" "udev" "video" ];
+    packages = with pkgs; [
+    autotiling
+    terminus_font
+    tldr
+    ];
   };
 
   # Allow unfree packages
@@ -83,33 +97,69 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-    wget
     firefox
-    vim
-    mpv
-    flameshot
-    krita
-    geany
-    neofetch
-    git
-    unrar
+    kate
+    alacritty
+    swaylock
+    swayidle
+    grim
+    slurp
     corectrl
-    spotify
-    discord
-    nomacs
-    ranger
-    ncdu
-    exa
-    fd
-    ripgrep
-    lutris
-    steam
-    htop
-    lm_sensors
+    wl-clipboard
+    mako
+    pulsemixer
+    pcmanfm
+    weechat
+    mangohud
+    flatpak
+    pwgen
+    retroarchFull
+    krita
     youtube-dl
-    fwupd
-    element-desktop 
+    ranger
+    qbittorrent
+    vulkan-tools
+    sshfs
+    tig
+    tree
+    btop
+    libappindicator
+    ncdu
+    mpv
+    gparted
+    lutris
+    geany
+    filezilla
+    unrar
+    unzip
+    nerdfonts
+    cpupower-gui
+    mupdf
+    nomacs
+    rofi-wayland
+    flameshot
+    git
+    foot
+    ripgrep
+    bash-completion
+    neofetch
+    fzf
+    fd
+    colordiff
+    exa
+    waybar
+    wayland
+    htop
+    tlp
+    sway
+    brightnessctl
+    bat
+    sshfs
+    pamixer
+    lsof
+    vim
+    wget
+    font-manager
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
@@ -123,20 +173,69 @@
   # List services that you want to enable:
 
   # Enable the OpenSSH daemon.
+  # Enable flatpak
+  # Enable fwupd
   services.openssh.enable = true;
+  services.flatpak.enable = true;
+  services.fwupd.enable = true;
 
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
   # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
+  networking.firewall.enable = false;
+
+  #Program options?
+  programs.sway = {
+    enable = true;
+  };
+  programs.corectrl = {
+    enable = true;
+  };
+
+  #sway configuration
+services.dbus.enable = true;
+  xdg.portal = {
+    enable = true;
+    wlr.enable = true;
+    extraPortals = [ pkgs.xdg-desktop-portal-wlr ];
+    gtkUsePortal = false;
+  };
+    ##tlp is a powerfull tool. Set battery charge and perf only missing gpustuff;
+
+#    services.tlp = {
+#    enable = true;
+#    settings = {
+#      CPU_SCALING_GOVERNOR_ON_BAT="powersave";
+#      CPU_SCALING_GOVERNOR_ON_AC="schedutil";
+
+      # The following prevents the battery from charging fully to
+      # preserve lifetime. Run `tlp fullcharge` to temporarily force
+      # full charge.
+      # https://linrunner.de/tlp/faq/battery.html#how-to-choose-good-battery-charge-thresholds
+#      START_CHARGE_THRESH_BAT0=75;
+#      STOP_CHARGE_THRESH_BAT0=80;
+
+      # 100 being the maximum, limit the speed of my CPU to reduce
+      # heat and increase battery usage:
+#      CPU_MAX_PERF_ON_AC=65;
+#      CPU_MAX_PERF_ON_BAT=50;
+#    };
+#  };
+  # Enable zramswap
+  zramSwap = {
+    enable = true;
+    algorithm = "zstd";
+  };
+  #check that services.zram-init-zram0.enable = true;
+  #it crated 7.5gb swap :) and check with swapon -s 7888448kb
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leavecatenate(variables, "bootdev", bootdev)
+  # on your system were taken. It‘s perfectly fine and recommended to leave
   # this value at the release version of the first install of this system.
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "22.05"; # Did you read the comment?
-
+  system.stateVersion = "22.10"; # Did you read the comment? Reading nix all day.
+  
 }
